9be331b29126a846793b153400c52ac0ded21824
Joel Davis <joeladavisdev@gmail.com>
2020-06-22 11:12:01 -0600

    POST working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 import os
 
 
@@ -11,6 +12,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+flask_bcrypt = Bcrypt(app)
 
 
 class User(db.Model):
@@ -31,6 +33,19 @@ users_schema = UserSchema(many=True)
 @app.route("/")
 def hello():
     return 'Hello World!'
+
+
+@app.route('/api/register', methods=["POST"])
+def register():
+    post_data = request.get_json()
+    username = post_data.get('username')
+    password = post_data.get('password')
+    hashed_password = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify('User Created!')
+
 
 # def sumNumber():
 #     print(2 + 2)
